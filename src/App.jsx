7b3db32f7538749d5f0f94587c8791329a196a88
jimhantrix <jimtekanyo@gmail.com>
2018-09-19 19:02:52 +0800

    update the URL

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,22 @@ import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 
 
 class App extends Component {
+
+constructor(props){
+	
+	super(props);
+
+		this.state = {
+
+		query: ''
+	
+	}
+}
+
+
+search(){
+	console.log('this.state', this.state);
+}
 	
 	render() {
 
@@ -18,8 +34,16 @@ class App extends Component {
 						<FormControl
 							type="text"
 							placeholder=" Search for an Artist"
+							query={this.state.query}
+							onChange={event => {this.setState({query: event.target.value})}}
+							onKeyPress={event =>{
+
+							 if (event.key === 'Enter'){
+							this.search()
+								}
+							}}
 						/>
-						<InputGroup.Addon>
+						<InputGroup.Addon onClick={() => this.search()}>
 						<Glyphicon glyph="search"></Glyphicon>
 					</InputGroup.Addon>
 					</InputGroup>
