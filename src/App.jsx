38d7879607c49c54f83fc4fb7cf6a353e328816b
jimhantrix <jimtekanyo@gmail.com>
2018-09-25 19:39:16 +0800

    updated css and profile content

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
-import  './App.css';
-import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
+import './App.css';
+import { FormGroup, FormControl, InputGroup, Glyphicon } from'react-bootstrap';
+
 import Profile from './Profile';
 
 class App extends Component {
 
-constructor(props){
-	
-	super(props);
+	constructor(props){
+		
+		super(props);
 
-		this.state = {
-		query: '',
-		artist: null
+			this.state = {
+			query: '',
+			artist: null
+		}
 	}
-}
+
 
 
 	search() {
@@ -22,9 +24,9 @@ constructor(props){
 
 		const BASE_URL = 'https://api.spotify.com/v1/search?';
 
-		const FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
+	   const FETCH_URL =`${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
 
-		console.log('FETCH_URL',FETCH_URL);
+		console.log('FETCH_URL', FETCH_URL);
 		fetch(FETCH_URL, {
 		method: 'GET'
 		})
@@ -32,7 +34,7 @@ constructor(props){
 		.then(response => response.json())
 		.then(json => {
 			const artist = json.artists.items[0];
-			console.log('artists', artist);
+			console.log('artist', artist);
 			this.setState({artist});
 		});
 	}
@@ -58,7 +60,7 @@ constructor(props){
 							 if (event.key === 'Enter'){
 							this.search()
 
-							 }
+							  }
 							}}
 						/>
 						<InputGroup.Addon onClick={() => this.search()}>
--- src/Profile.jsx
+++ src/Profile.jsx
@@ -4,7 +4,9 @@ import './App.css';
 class Profile extends Component {
 	
 	render(){
-		let artist = {name: '', followers: { total: ''},images: [{url: ''}], genres: []};
+
+		let artist = { name: '', followers: { total: ''},images: [{url: ''}], genres: []};
+
 		artist = this.props.artist !== null ? this.props.artist: artist;
 		
 
@@ -13,27 +15,33 @@ class Profile extends Component {
 			<div className="profile">
 				<img
 
-				alt="Profile"
-				className="profile-img"
-				src={artist.image[0].url}
-
+					alt="Profile"
+					className="profile-img"
+					src={artist.images[0].url}
 				/>
-				<div>{artist.name}</div>
-				<div>{artist.followers.total}</div>
-				<div>
 
-					{
-						artist.genres.map((genre, k)=> {
 
-						genre = genre !== artist.genres[artist.genres.length-1] ? `${genre},`:`&${genre}`
-						return(
+				<div className="profile-info">
 
-						<span key ={k}>{genre}</span>
+					<div className="profile-name">{artist.name}</div>
 
-							)
-						})
-					}
+					<div className="prifle-followers">{artist.followers.total} followers</div>
 
+						<div className ="profile-genres">
+
+							{
+								artist.genres.map((genre, k)=> {
+
+									genre = genre !== artist.genres[artist.genres.length-1] ? `${genre},`:`& ${genre}`
+
+									return(
+
+										<span key ={k}>{genre}</span>
+
+									)
+								})
+							}
+					</div>
 				</div>
 			</div>
 		)
